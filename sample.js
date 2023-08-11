69c04b2d429e4752934f7aac449b62b4ea34aa72
Sahana M <sahanasaha1505@gmail.com>
2023-08-11 14:54:18 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,11 @@
+window.alert("About to witness the most creative page")
+
+function p1(){
+    document.getElementById("Hi").innerHTML ="HI! This is Sahana"
+}
+function scrollToSection2(){
+    document.getElementById("Hi").innerHTML ="B-Tech AI&DS"
+}
+function scrollToSection3(){
+    document.getElementById("Hi").innerHTML ="Please don't"
+}
